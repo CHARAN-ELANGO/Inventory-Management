cbe1977e9532ed6e37a46670a4bca7d555c726aa
CHARAN-ELANGO <charanelango333@gmail.com>
2024-10-03 13:21:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1406'
+app.config['MYSQL_PASSWORD'] = 'Charanelangovan333@'
 app.config['MYSQL_DB'] = 'inventory_management'
 
 mysql = MySQL(app)
